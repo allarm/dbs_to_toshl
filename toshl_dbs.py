4ede1d409fa0f76f535dbb6c7e327dcf9c411bb9
andrey_chertolyas <andrey.chertolyas@airbnb.com>
2018-07-22 12:56:19 +0800

    2nd

--- toshl_dbs.py
+++ toshl_dbs.py
@@ -2,8 +2,10 @@
 
 import re
 from pprint import pprint
+import yaml
 
 input_file = '/Users/achertolyas/git/toshl_dbs/May-June credit.txt'
+categories_file = '/Users/achertolyas/git/toshl_dbs/categories.yaml'
 
 # p = re.compile('^\d\d\s\D{3}\s\d{4}')
 p = re.compile('^(?P<date_str>'
@@ -19,11 +21,20 @@ p = re.compile('^(?P<date_str>'
                 ')'
 )
 
+with open(categories_file, 'r') as f:
+    categories = yaml.load(f)
+
 with open(input_file, 'r') as f:
     for x in f:
-        if len(x.strip()) > 1:
-            parsed=p.match(x.strip())
+        x = x.strip()
+        if len(x) > 1:
+            parsed = p.match(x)
             if parsed:
-                # print(x.strip())
-                # print(len(x))
-                pprint(parsed.groupdict())
+                for y in categories['regexps']:
+                    pc = re.compile(y['regexp'])
+                    if pc.match(parsed.groupdict()['description']):
+                        print("Matched: {}, category: {}, tags: {}".format(y['regexp'], y['category'], y['tags']))
+                        pprint(parsed.groupdict())
+                    else:
+                        print("No match")
+                    # pprint(parsed.groupdict())
